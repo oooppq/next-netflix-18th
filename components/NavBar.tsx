8b78fc6734d5a902be5655f109776b8726882208
DaegyunOh <oooppq@naver.com>
2023-11-06 21:05:18 +0900

    style: nav bar 스타일 적용

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,5 +1,34 @@
+'use client';
+
+import NavBarButton from '@/components/NavBarIcon';
+import {
+  HomeIcon,
+  SearchIcon,
+  ComingsoonIcon,
+  DownloadIcon,
+  MoreIcon,
+} from '@/public/images';
+
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <div className="w-full px-7 fixed bottom-0 h-12 flex bg-[#121212] justify-between items-end pb-[0.3rem]">
+      <NavBarButton name="Home" isCurrentPage={true}>
+        <HomeIcon />
+      </NavBarButton>
+      <NavBarButton name="Search" isCurrentPage={true}>
+        <SearchIcon />
+      </NavBarButton>
+      <NavBarButton name="Coming Soon" isCurrentPage={true}>
+        <ComingsoonIcon />
+      </NavBarButton>
+      <NavBarButton name="Downloads" isCurrentPage={true}>
+        <DownloadIcon />
+      </NavBarButton>
+      <NavBarButton name="More" isCurrentPage={true}>
+        <MoreIcon />
+      </NavBarButton>
+    </div>
+  );
 };
 
 export default NavBar;
