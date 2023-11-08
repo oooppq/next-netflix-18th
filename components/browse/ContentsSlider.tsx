a097775305e24730e6e309f353f4bb2a36f837e5
DaegyunOh <oooppq@naver.com>
2023-11-08 16:52:47 +0900

    feat: content slider 마우스로 drag 구현
    기존에는 횡이동 스크롤로만 움직일 수 있었으나, 기능상 마우스로 드래그하는 것이 꼭 필요하기 때문에
    useState와 useRef를 사용하여 마우스 drag를 구현했다. 그 과정에서 해당 컴포넌트를 client component로
    변경했다.

--- components/browse/ContentsSlider.tsx
+++ components/browse/ContentsSlider.tsx
@@ -1,5 +1,8 @@
+'use client';
+
 import ContentElement from '@/components/browse/ContentElement';
 import { TContent } from '@/types';
+import { useRef, useState, MouseEvent } from 'react';
 
 interface ContentsSliderProps {
   title: string;
@@ -7,10 +10,42 @@ interface ContentsSliderProps {
 }
 
 const ContentsSlider = ({ title, contents }: ContentsSliderProps) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [dragging, setDragging] = useState<boolean>(false);
+  const [clickPoint, setClickPoint] = useState<number>(0);
+  const [scrollLeft, setScrollLeft] = useState<number>(0);
+
+  const handleMouseDownEvent = (e: MouseEvent<HTMLDivElement>) => {
+    setDragging(true);
+    if (containerRef.current) {
+      setClickPoint(e.pageX);
+      setScrollLeft(containerRef.current.scrollLeft);
+    }
+  };
+
+  const handleMouseMoveEvent = (e: MouseEvent<HTMLDivElement>) => {
+    if (!dragging) return;
+
+    e.preventDefault();
+
+    if (containerRef.current) {
+      const walk = e.pageX - clickPoint;
+
+      containerRef.current.scrollLeft = scrollLeft - walk;
+    }
+  };
+
   return (
     <div className="">
       <h3 className="text-[20.921px] font-bold ml-4 text-white">{title}</h3>
-      <div className="flex mt-2 mb-5 overflow-auto scroll-smooth scroll scroll-hide">
+      <div
+        className="flex mt-2 mb-5 overflow-auto scroll-smooth scroll scroll-hide"
+        ref={containerRef}
+        onMouseDown={handleMouseDownEvent}
+        onMouseLeave={() => setDragging(false)}
+        onMouseUp={() => setDragging(false)}
+        onMouseMove={handleMouseMoveEvent}
+      >
         {contents.map((content, idx) => (
           <ContentElement
             key={`${content.id}${content.title}`}
