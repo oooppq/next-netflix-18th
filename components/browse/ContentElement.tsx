b84ffc7e568e924338b1da3731e7f65e5ed39280
DaegyunOh <oooppq@naver.com>
2023-11-08 16:15:41 +0900

    style: content slider 스타일 적용

--- components/browse/ContentElement.tsx
+++ components/browse/ContentElement.tsx
@@ -4,12 +4,26 @@ import Link from 'next/link';
 
 interface ContentElementProps {
   content: TContent;
+  lastFlag: boolean;
 }
 
-const ContentElement = ({ content }: ContentElementProps) => {
+const ContentElement = ({ content, lastFlag }: ContentElementProps) => {
+  const width = 103;
+  const height = 161;
+  const url = `https://image.tmdb.org/t/p/w154`;
+
   return (
-    <Link href="" className="">
-      <Image src={content.poster_path} alt={content.title} className="" />
+    <Link
+      href={`?id=${content.id}`}
+      className={`w-[103px] h-[161px] flex ${lastFlag ? 'mr-3' : 'mr-2'}`}
+    >
+      <Image
+        src={url + content.poster_path}
+        alt={content.title}
+        className="min-w-[103px] h-[161px]"
+        width={width}
+        height={height}
+      />
     </Link>
   );
 };
--- components/browse/ContentsSlider.tsx
+++ components/browse/ContentsSlider.tsx
@@ -8,14 +8,17 @@ interface ContentsSliderProps {
 
 const ContentsSlider = ({ title, contents }: ContentsSliderProps) => {
   return (
-    <div className="">
-      <h3 className="">{title}</h3>
-      {contents.map((content) => (
-        <ContentElement
-          key={`${content.id}${content.title}`}
-          content={content}
-        />
-      ))}
+    <div className="ml-3">
+      <h3 className="text-[20.921px] font-bold mx-1 text-white">{title}</h3>
+      <div className="flex mt-2 mb-5 overflow-auto scroll-smooth scroll">
+        {contents.map((content, idx) => (
+          <ContentElement
+            key={`${content.id}${content.title}`}
+            content={content}
+            lastFlag={idx === contents.length - 1}
+          />
+        ))}
+      </div>
     </div>
   );
 };
