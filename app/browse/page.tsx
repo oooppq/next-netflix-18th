bb36436ae31201fd4b8d9be798b4e4898da2cd3a
DaegyunOh <oooppq@naver.com>
2023-11-08 21:04:43 +0900

    feat: home top 기능 구현

--- app/browse/page.tsx
+++ app/browse/page.tsx
@@ -4,6 +4,10 @@ import HomeTop from '@/components/browse/HomeTop';
 import { TContent } from '@/types';
 import React from 'react';
 
+interface BrowseProps {
+  searchParams: { [key: string]: string | undefined };
+}
+
 const CONTENTS: TContent[] = [
   {
     id: 1,
@@ -37,16 +41,31 @@ const CONTENTS: TContent[] = [
   },
 ];
 
-const Browse = () => {
+const Browse = ({ searchParams }: BrowseProps) => {
+  const defualtData = {
+    rank: 1,
+    category: 'Nigeria Today',
+    posterPath: '/1E5baAaEse26fej7uHcjOgEE2t2.jpg',
+  };
   return (
     <div className="bg-black w-full h-full relative">
       <HomeNavBar />
       <HomeTop
-        posterPath="/1E5baAaEse26fej7uHcjOgEE2t2.jpg"
-        rank={1}
-        category="Nigeria Today"
+        posterPath={searchParams.posterPath || defualtData.posterPath}
+        rank={
+          searchParams.rank
+            ? Number(searchParams.rank)
+            : searchParams.posterPath
+            ? null
+            : defualtData.rank
+        }
+        category={searchParams.category || defualtData.category}
       />
-      <ContentsSlider title="Top 10 in Nigeria Today" contents={CONTENTS} />
+      <ContentsSlider
+        title="Nigeria Today"
+        isRanking={true}
+        contents={CONTENTS}
+      />
     </div>
   );
 };
--- components/browse/HomeTop.tsx
+++ components/browse/HomeTop.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 interface HomeTopProps {
   posterPath: string;
-  rank?: number;
+  rank: number | null;
   category: string;
 }
 
