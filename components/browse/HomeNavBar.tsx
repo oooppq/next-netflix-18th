5a008ddd4d3aaab8d99d0d6d4b6a7f1ec6ac8afd
DaegyunOh <oooppq@naver.com>
2023-11-08 14:37:05 +0900

    style home navbar style 적용 완료

--- components/browse/HomeNavBar.tsx
+++ components/browse/HomeNavBar.tsx
@@ -1,5 +1,14 @@
+import { NetflixIcon } from '@/public/images';
+
 const HomeNavBar = () => {
-  return <div>HomeNavBar</div>;
+  return (
+    <div className="flex justify-between items-center px-[18px] mt-6 text-[17px] text-white">
+      <NetflixIcon />
+      <button className="px-[2px]">TV Shows</button>
+      <button className="px-[2px]">Movies</button>
+      <button className="px-[2px]">My List</button>
+    </div>
+  );
 };
 
 export default HomeNavBar;
--- public/images/index.ts
+++ public/images/index.ts
@@ -3,3 +3,4 @@ export { default as DownloadIcon } from './download-icon.svg';
 export { default as HomeIcon } from './home-icon.svg';
 export { default as MoreIcon } from './more-icon.svg';
 export { default as SearchIcon } from './search-icon.svg';
+export { default as NetflixIcon } from './netflix-mini-logo.svg';
