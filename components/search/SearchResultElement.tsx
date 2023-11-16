51e9c71f07d2d823f7ffc246d6f02dccf8fa74b6
DaegyunOh <oooppq@naver.com>
2023-11-17 01:44:03 +0900

    chore: 검색결과에도 detail 페이지 디렉토리 변화 적용

--- components/search/SearchResultElement.tsx
+++ components/search/SearchResultElement.tsx
@@ -21,7 +21,7 @@ const SearchResultElement = ({ movie }: SearchResultElementProps) => {
   const baseUrl = 'https://image.tmdb.org/t/p/original';
   const defaultImageUrl =
     'https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-38-picture-grey-c2ebdbb057f2a7614185931650f8cee23fa137b93812ccb132b9df511df1cfac.svg';
-  let clickUrl = `/movies/${movie.id}/${movie.poster_path}`;
+  let clickUrl = `/movies/${movie.id}`;
   return (
     <Link href={clickUrl}>
       <div className="relative">
